32e03295b579f56f7aa4a2d72671f103ab76a04d
Robin Hans Scholtz <scholtz@campus.tu-berlin.de>
2018-06-04 20:12:16 +0200

    Restored human_overlay.py

--- code/hrc_morse/src/human_overlay.py
+++ code/hrc_morse/src/human_overlay.py
@@ -3,20 +3,11 @@ from morse.core.overlay import MorseOverlay
 from morse.core import status
 import threading
 import time
-import rospy
-from std_msgs.msg import String
 
 from std_srvs.srv import Trigger, TriggerResponse
 
 
 class HumanControlAndMonitor(MorseOverlay):
-    countGrasp = 0
-    countReset = 10
-
-    def debug(self,string):
-        if (1==1):
-            rospy.loginfo(string)
-
     def __init__(self, overlaid_object):
         # Call the constructor of the parent class
         MorseOverlay.__init__(self, overlaid_object)
@@ -28,82 +19,6 @@ class HumanControlAndMonitor(MorseOverlay):
         #self.overlaid_object.is_wr = False  # has human warned the robot
         #self.overlaid_object.is_gr = False  # has human grasped the object
         #self.overlaid_object.is_ag = False  # has human attempted to grasp the object
-
-	#self.overlaid_object.warn_robot
-	#warn_robot(self)	#does not work that way!
-        
-    @ros_service(type=Trigger, name='control')
-    def control(self):
-        # Initializes the humanController and starts the callback-loop
-        rospy.Subscriber("Controller", String, self.human_control)
-        rospy.spin()
-
-    def human_control(self, data):#TODO
-        # Control human
-        # special actions
-        if list(data.data)[0]=="1":
-            # triangle-button
-            self.debug("LOOKING AROUND")
-            rospy.loginfo("DATA: %s", data.data)
-            self.look_around()
-        if list(data.data)[1]=="1":
-            # circle-button
-            self.debug("WALK AWAY")
-            rospy.loginfo("DATA: %s", data.data)
-            self.walk_away()
-        if (list(data.data)[2]=="1" and self.countGrasp==0):
-            # x-button
-            self.debug("SUCCESSFUL GRASP")
-            rospy.loginfo("DATA: %s", data.data)
-            self.grasp()
-            self.countGrasp = self.countReset
-        if list(data.data)[3]=="1":
-            # square-button
-            self.debug("WARNING THE ROBOT")
-            rospy.loginfo("DATA: %s", data.data)
-            self.warn_robot()
-####################
-        if (list(data.data)[4]=="1" and list(data.data)[6]=="1"):
-            # l1+l2
-            self.debug("LOOKING TO THE LEFT")
-            rospy.loginfo("DATA: %s", data.data)
-            self.overlaid_object.look_left()
-        if (list(data.data)[5]=="1" and list(data.data)[7]=="1"):
-            # r1+r2
-            self.debug("LOOKING TO THE RIGHT")
-            rospy.loginfo("DATA: %s", data.data)
-            self.overlaid_object.look_right()
-        if (list(data.data)[4]=="1" and list(data.data)[5]=="1"):
-            # l1+r1
-            self.debug("LOOKING UP")
-            rospy.loginfo("DATA: %s", data.data)
-            self.overlaid_object.look_up()
-        if (list(data.data)[6]=="1" and list(data.data)[7]=="1"):
-            # l2+r2
-            self.debug("LOOKING DOWN")
-            rospy.loginfo("DATA: %s", data.data)
-            self.overlaid_object.look_down()
-####################
-        if list(data.data)[19]=="1":
-            # right analogstick - turning clockwise
-            self.debug("BENDING DOWN")
-            rospy.loginfo("DATA: %s", data.data)
-            self.bow()
-
-        # for DEBUG-Purposes
-        if list(data.data)[9]=="1":
-            #for debug-purposes
-            self.debug("RESETING THE HUMAN")
-            rospy.loginfo("DATA: %s", data.data)
-            self.reset()
-        # different counts if a sequence should only be played once
-        if self.countGrasp>0:
-            self.countGrasp = self.countGrasp - 1
-
-
-    def look_left(self):
-        self.overlaid_object.look_left()
-        return TriggerResponse(True, 'human: look left')
 
     @ros_service(type=Trigger, name='is_ov')
     def is_ov(self):
