ac950aea79346ce7bfbfd1340543aaa9ef65d43d
Elia Kargruber <kargruber@campus.tu-berlin.de>
2019-04-18 19:26:39 +0200

    (RuleMonitorNode): Added coloured print for   all subtasks the same case -> tested and working  -   WIP for complex subtask rules

--- code/ros_ws/src/hrc_ros/src/rule_monitor/ruleMonitorNode.cpp
+++ code/ros_ws/src/hrc_ros/src/rule_monitor/ruleMonitorNode.cpp
@@ -7,6 +7,9 @@
 
 #include <ros/ros.h>
 #include<stdlib.h>
+#include <ros/package.h>
+
+#include <string>
 
 
 #include<ros/callback_queue.h>
@@ -15,6 +18,9 @@
 
 // generic service includes 
 #include <std_srvs/Trigger.h>
+
+// includes for Json parsing
+#include <helper_functions/Json_parser.h>
 
 
 
@@ -31,28 +37,114 @@ const std::string green("\033[1;32m");
 const std::string blue("\033[1;34m");
 const std::string normal("\033[1;37m");	
 
+// variables and structs to parse Json 
+task_set current_task_set;
+success_combo success_criteria_read;
+
+
+
 //#################### function declarations ##################### 
+std::string mapintToString_colour(int int_value, std::string & print_string);
 bool display_task_rules_server(hrc_ros::DisplayTaskRuleRequest &req,hrc_ros::DisplayTaskRuleResponse &res);
 void clear_screen(void);
 
 // ########## Function implementations ##########################
+
+std::string mapintToString_colour(int int_value, std::string & print_string){
+
+	 std::string return_string = normal;  
+
+	if (int_value == 1) {// red
+		return_string = red;
+		print_string = " red ";  
+	} else if (int_value ==2){ // green
+		return_string = green; 
+		print_string = " green ";
+	} else if (int_value == 3){
+		return_string = blue;
+		print_string = " blue ";
+	}
+	 
+	return return_string; 
+}
+
 bool display_task_rules_server(hrc_ros::DisplayTaskRuleRequest &req,hrc_ros::DisplayTaskRuleResponse &res){
 
+	int task_counter = req.task_counter; 
+
+
 	clear_screen(); 
-	cout << normal << "This is a task_rule " << endl << endl; 
-	cout << red   << " red text" << endl; 
-	cout << green << "green text" << endl; 
-	cout << blue  << "blue text" << endl; 
-	cout << blue << "blue tesxt" << normal << "normal text" << red << " red text" << green << " green text " << endl; 
 
 
 	//### parse task_rule file 
 
+// Call the parse function to parse the scenario definition file 
+	boost::property_tree::ptree testscenario_pt;
+	string pkg_path1 = ros::package::getPath("hrc_ros");
+	std::ifstream jsonFiletask(pkg_path1 + "/../../../configs/IE_task_config.json");
+
+	try {
+		boost::property_tree::read_json(jsonFiletask, testscenario_pt);
+	} catch(boost::property_tree::json_parser::json_parser_error &e) {
+		ROS_FATAL("Cannot parse the message to Json. Error: %s", e.what());
+		res.success = false;
+		return false;
+	}
 
 
-	// wait for certain time, then clear the screen and return true to start the experiment
-	ros::Duration(5).sleep();
-	clear_screen();  
+    stringstream ss;
+    ss << task_counter;
+    string task_str = ss.str(); 
+
+	current_task_set = read_task_set(task_str,testscenario_pt);
+	int subtask_number_current_task = current_task_set.subtask_quantity; 
+
+
+
+
+	if (current_task_set.all_set == true){ // rules are the same for all subtasks 
+		int subtask_counter = 1; 
+		cout << endl << endl << endl << normal << "Task #: " << task_counter << "		Subtasks: " << current_task_set.subtask_quantity << endl <<"------------------------------------------------------------------" << endl << endl << endl << endl; 
+		cout << "For all subtasks : "<< endl << "-----------------"<< endl << endl << endl;  
+		for ( int obj = 1; obj <=3; obj++){
+			// ## get strings for success_criteria request
+			stringstream ss_task_counter;  
+			stringstream ss_subtask_counter; 
+
+			ss_task_counter << task_counter;
+			ss_subtask_counter << subtask_counter;
+			
+			
+			string subtask_str = ss_subtask_counter.str();
+			string object_str  = object_int_to_str(obj);
+
+			// ## determine subtask success state 
+			try {
+			success_criteria_read = get_success_criteria(task_str,subtask_str,object_str,testscenario_pt);
+			} catch(boost::property_tree::json_parser::json_parser_error &e) {
+				ROS_FATAL("Cannot parse the message to Json. Error: %s", e.what());
+				return false;
+			}
+
+			string obj_print_string; 
+			string container_print_string; 
+			const std::string obj_colour = mapintToString_colour(obj,obj_print_string); 
+			const std::string container_colour = mapintToString_colour(success_criteria_read.tray, container_print_string );
+			
+			cout << "			Place " << obj_colour << obj_print_string << normal << " object into " << container_colour << container_print_string << normal << " container " << endl << endl; 
+		} 
+
+		// wait for certain time, then clear the screen and return true to start the experiment
+		ros::Duration(15).sleep();
+		clear_screen();  
+	} else { // different subtask rules 
+		  cout << endl << endl << endl << normal << "Task #: " << task_counter << "		Subtasks: " << current_task_set.subtask_quantity << endl <<"------------------------------------------------------------------" << endl << endl << endl << endl; 
+		  cout << " different subtasks not supported yet -> available soon :-) " << endl; 
+		  // wait for certain time, then clear the screen and return true to start the experiment
+		  ros::Duration(5).sleep();
+		  clear_screen();
+	}
+
 
 
 
