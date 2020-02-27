693b959bd82dd5b9eab56285367631b2723023c8
Orhan Can Görür <cangorur@Orhans-MacBook-Pro.local>
2020-02-27 23:10:18 +0100

    KL divergence analysis for human model vs real human data match tests

--- code/results/userStudies_exp1_results/human_model_validation.py
+++ code/results/userStudies_exp1_results/human_model_validation.py
@@ -6,9 +6,31 @@ Created on Mon Feb 24 23:45:29 2020
 @author: cangorur
 """
 
-import sklearn
+import sklearn.metrics as mtr
+import pandas as pd
+import os
+
+user_obs = []
+taskID_arr = []
+
+def readObs():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    df = pd.read_excel(dir_path + '/../userStudies_exp2_results/tests/analysis_objective.xlsx', sheet_name='human_observables', sep='\s*,\s*')
+    part_id = 4
+    total_subjects = 14
+    for i in range(total_subjects-1):
+        part_temp_obs = df['Obs_' + str(part_id)].values.tolist()
+        part_temp_obs = [x for x in part_temp_obs if str(x) != 'nan']
+        user_obs.append(part_temp_obs)
+        part_tasks = df['task_id_' + str(part_id)].values.tolist()
+        part_tasks = [x for x in part_tasks if str(x) != 'nan']
+        taskID_arr.append(part_tasks)
+        part_id += 1
+    return user_obs, taskID_arr
 
 if __name__=='__main__':
-    score = sklearn.metrics.mutual_info_score([0,1],[1,0])
+    
+    user_obs,taskID_arr = readObs()
+    score = mtr.mutual_info_score(user_obs[1][:256],user_obs[2])
     print(score)
     
